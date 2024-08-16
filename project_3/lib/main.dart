7a04f5b9f89ea572078a971ebde45f54666d6637
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-16 14:51:57 +0300

    Done LinearProgressIndicator

--- project_3/lib/main.dart
+++ project_3/lib/main.dart
@@ -22,15 +22,18 @@ class MainApp extends StatelessWidget {
                 ]),
                 const Text('Hello World!'),
                 // Simple progress indicators package with solid colors and gradients
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 90),
-                  child: LinearProgressIndicator(
-                    value: 0.5,
-                    color: Color.fromRGBO(236, 192, 85, 1),
-                    minHeight: 14,
-                    borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(10),
-                        bottomRight: Radius.circular(10)),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 90),
+                  child: Transform(
+                    transform: Matrix4.skewX(-0.5),
+                    child: const LinearProgressIndicator(
+                      value: 0.5,
+                      color: Color.fromRGBO(236, 192, 85, 1),
+                      minHeight: 14,
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(10),
+                          bottomRight: Radius.circular(10)),
+                    ),
                   ),
                 ),
               ],
