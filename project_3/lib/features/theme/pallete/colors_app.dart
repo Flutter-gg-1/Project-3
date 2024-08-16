2c5de6c40936a55e40697ae2f3ededa08805760a
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-16 08:44:18 +0300

    UPD ColorPallete

--- project_3/lib/features/theme/pallete/colors_app.dart
+++ project_3/lib/features/theme/pallete/colors_app.dart
@@ -2,10 +2,15 @@ import 'package:flutter/material.dart';
 
 class ColorPallete {
   static const Color colorDarkOrange = Color.fromRGBO(235, 159, 74, 1);
+  static const Color colorLightOrange = Color.fromRGBO(236, 192, 85, 1);
   static const Color colorSky = Color.fromRGBO(119, 178, 159, 1);
   static const Color colorBlack = Color.fromRGBO(0, 0, 0, 1);
   static const Color colorLightGrey = Color.fromRGBO(251, 245, 242, 1);
   static const Color colorDarkGrey = Color.fromRGBO(196, 196, 196, 40);
   static const Color colorWhite = Color.fromRGBO(255, 255, 255, 1);
-  static const Color colorGreen = Color.fromRGBO(114, 191, 199, 1);
+  static const Color colorDarkGreen = Color.fromRGBO(114, 191, 199, 1);
+  static const Color colorLightGreen = Color.fromRGBO(65, 172, 120, 1);
+  static const Color colorBlue = Color.fromRGBO(2, 161, 251, 1);
+  static const Color colorPurple = Color.fromRGBO(171, 112, 223, 1);
+  static const Color colorLightRed = Color.fromRGBO(220, 63, 0, 1);
 }
