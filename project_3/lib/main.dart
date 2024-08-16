0ae3547f26e6992c3a265d25294099ecd75a5565
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-16 14:44:13 +0300

    ADD Padding on LinearProgressIndicator

--- project_3/lib/main.dart
+++ project_3/lib/main.dart
@@ -22,14 +22,16 @@ class MainApp extends StatelessWidget {
                 ]),
                 const Text('Hello World!'),
                 // Simple progress indicators package with solid colors and gradients
-                const LinearProgressIndicator(
-                  value: 0.5,
-                  color: Color.fromRGBO(236, 192, 85, 1),
-                  minHeight: 14,
-                  
-                  borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(10),
-                      bottomRight: Radius.circular(10)),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 90),
+                  child: LinearProgressIndicator(
+                    value: 0.5,
+                    color: Color.fromRGBO(236, 192, 85, 1),
+                    minHeight: 14,
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(10),
+                        bottomRight: Radius.circular(10)),
+                  ),
                 ),
               ],
             ),
