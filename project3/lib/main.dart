20bfde15580fcd4bba1fd3268d9b576d58f7f992
dev <nooof-2017a@hotmail.com>
2024-08-17 16:01:49 +0300

    create profile page

--- project3/lib/main.dart
+++ project3/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Flutter App project3 ',
       home: Scaffold(
         body: Login(),
       ),
