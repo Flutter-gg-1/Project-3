c79f849b105ec8ac751f9d5b92341aed7eb5a7e5
Amjad Elahi <amjadelahi07@gmail.com>
2024-08-17 20:26:02 +0300

    implements setting  screen

--- lib/src/setting.dart
+++ lib/src/setting.dart
@@ -7,7 +7,24 @@ class Setting extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
       backgroundColor: const Color(0xfffbf5f2),
-      body: Text("data"),
+      appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.transparent,
+          bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(75),
+              child: Container(
+                height: 71,
+                width: 428,
+                color: const Color(0xffE5E5E5),
+                child: const Center(
+                  child: Text(
+                    "Setting",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 0, 0, 0), fontSize: 30),
+                  ),
+                ),
+              ))),
+      body: const Text(""),
     );
   }
 }
