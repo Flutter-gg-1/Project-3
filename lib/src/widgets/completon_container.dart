7ee734c45a0fe2a224029249dc44b01f97a4c16d
yara albouq <yaramalbouq@gmail.com>
2024-08-18 04:04:51 +0300

    last modifiy

--- lib/src/widgets/completon_container.dart
+++ lib/src/widgets/completon_container.dart
@@ -28,38 +28,42 @@ Widget completionContainer({
       ),
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 15),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const Text(
-              "Unit 1",
-              style: TextStyle(fontSize: 28),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15),
-              child: Row(
-                children: [
-                  const Icon(
-                    FluentIcons.crown_20_filled,
-                    size: 35,
-                    color: Color(0xffECC055),
-                  ),
-                  Expanded(
-                    child: LinearProgressIndicator(
-                        minHeight: 10,
-                        backgroundColor: const Color(0xffC4C4C4),
-                        color: const Color(0xffECC055),
-                        value: value,
-                        borderRadius: const BorderRadius.only(
-                          topRight: Radius.circular(10),
-                          bottomRight: Radius.circular(10),
-                        )),
-                  )
-                ],
+        child: Stack(children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                "Unit 1",
+                style: TextStyle(fontSize: 28),
               ),
-            )
-          ],
-        ),
+              
+              Padding(
+                padding: const EdgeInsets.only(left: 36, top: 10, bottom: 10),
+                child: SizedBox(
+                  width: 130,
+                  child: LinearProgressIndicator(
+                      minHeight: 13,
+                      backgroundColor: const Color(0xffC4C4C4),
+                      color: const Color(0xffECC055),
+                      value: value,
+                      borderRadius: const BorderRadius.only(
+                        topRight: Radius.circular(10),
+                        bottomRight: Radius.circular(10),
+                      )),
+                ),
+              ),
+            ],
+          ),
+          const Positioned(
+            bottom: 2,
+            left: 5,
+            child: Icon(
+              FluentIcons.crown_20_filled,
+              size: 35,
+              color: Color(0xffECC055),
+            ),
+          )
+        ]),
       ),
     ),
   );
