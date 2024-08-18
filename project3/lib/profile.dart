21cd805dcfca0f3046b62ed0d659360be246ae5d
unknown <lindaalmakramy@gmail.com>
2024-08-18 09:04:06 +0300

    added forth page

--- project3/lib/profile.dart
+++ project3/lib/profile.dart
@@ -33,58 +33,257 @@ class Profile extends StatelessWidget {
           padding:  const EdgeInsets.fromLTRB(30, 20, 30, 0),
           child: ListView(
             children: [
-              Row(
-                children: [
-                  const Text("Logical reasoning", style: TextStyle(fontSize: 22),),
-                  const Spacer(),
-                  Image.asset("assets/images/crown.png"),
-                  const Text("  18/40", style: TextStyle(color: Color.fromARGB(160, 0, 0, 0), fontSize: 18),),
-                ],
-              ),
-              const SizedBox(height: 10),
               const Row(
                 children: [
-                  UnitCard(percentage: 0.4), 
-                  Spacer(),
-                  LockedCard(),                      
+                 ProfileSection()
                 ],
               ),
-              const SizedBox(height: 10),
+              const Divider(
+                height: 20,
+                thickness: 2, 
+            ),
+            Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: const Color.fromARGB(29, 0, 0, 0), width: 2),
+                    borderRadius: BorderRadius.circular(20),
+                    color: const Color.fromARGB(0, 255, 255, 255),
+                  ) ,
+                  child: Row(children: [
+                    Image.asset("assets/images/party.png",),
+                    const Text("  Friends updates"),
+                    const SizedBox(width: 95),
+                    const Icon(Icons.arrow_forward_ios_rounded, size: 16, color: Color.fromARGB(127, 0, 0, 0)),
+
+                  ],),
+                ),
+            ],),
+            const SizedBox(height: 20),
+            const Text("Statistic", style: TextStyle(fontSize: 20,),),
               Row(
-                children: [
-                  const Text("Artistic thinking", style: TextStyle(fontSize: 22),),
-                  const Spacer(),
-                  Image.asset("assets/images/crown.png"),
-                  const Text("  35/40", style: TextStyle(color: Color.fromARGB(160, 0, 0, 0), fontSize: 18),),
-                ],
-              ),
-              const SizedBox(height: 10),
-              const Row(
-                children: [
-                  UnitCard(percentage: 0.8), 
-                  Spacer(),
-                  LockedCard(),                      
-                ],
-              ),
-              const SizedBox(height: 10),
-              Row(
-                children: [
-                  const Text("Verbal skills", style: TextStyle(fontSize: 22),),
-                  const Spacer(),
-                  Image.asset("assets/images/crown.png"),
-                  const Text("  3/40", style: TextStyle(color: Color.fromARGB(160, 0, 0, 0), fontSize: 18),),
-                ],
-              ),
-              const SizedBox(height: 10),
-              const Row(
-                children: [
-                  UnitCard(percentage: 0.3), 
-                  Spacer(),
-                  LockedCard(),                      
+              children: [
                 
-                ],
-              ), 
-              
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: const Color.fromARGB(29, 0, 0, 0), width: 2),
+                    borderRadius: BorderRadius.circular(20),
+                    color: const Color.fromARGB(0, 255, 255, 255),
+                  ) ,
+                  child: Row(children: [
+                    Image.asset("assets/images/fire.png",),
+                    const SizedBox(width: 5),
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,  
+                      children: [
+                      Text(
+                      '3',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      'Day Streak',
+                      style: TextStyle(
+                        fontSize: 10,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    ],),
+                    const SizedBox(width: 24),
+                  ],),
+                ),
+                const Spacer(),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: const Color.fromARGB(29, 0, 0, 0), width: 2),
+                    borderRadius: BorderRadius.circular(20),
+                    color: const Color.fromARGB(0, 255, 255, 255),
+                  ) ,
+                  child: Row(children: [
+                    Image.asset("assets/images/spark.png",),
+                    const SizedBox(width: 5),
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,  
+                      children: [
+                      Text(
+                      '1432',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      'Total XP',
+                      style: TextStyle(
+                        fontSize: 10,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    ],),
+                    const SizedBox(width: 40),
+                  ],),
+                ),
+            ],),
+             const SizedBox(height: 20),
+              Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: const Color.fromARGB(29, 0, 0, 0), width: 2),
+                    borderRadius: BorderRadius.circular(20),
+                    color: const Color.fromARGB(0, 255, 255, 255),
+                  ) ,
+                  child: Row(children: [
+                    Image.asset("assets/images/golden_medal.png",),
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,  
+                      children: [
+                      Text(
+                      'Bronze',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      'Current League',
+                      style: TextStyle(
+                        fontSize: 10,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    ],),
+                  ],),
+                ),
+                const Spacer(),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: const Color.fromARGB(29, 0, 0, 0), width: 2),
+                    borderRadius: BorderRadius.circular(20),
+                    color: const Color.fromARGB(0, 255, 255, 255),
+                  ) ,
+                  child: Row(children: [
+                    Image.asset("assets/images/medal.png",),
+                    const SizedBox(width: 5),
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,  
+                      children: [
+                      Text(
+                      '0',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      'Top 3 Finishes',
+                      style: TextStyle(
+                        fontSize: 10,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    ],),
+                  ],),
+                ),
+            ],),
+            const SizedBox(height: 20),
+            const Row(children: [
+              Text("Friends", style: TextStyle(fontSize: 20),),
+              Spacer(),
+              Text("ADD FRIENDS", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 2, 160, 251)),),
+            ],),
+            
+            Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: const Color.fromARGB(29, 0, 0, 0), width: 2),
+                    borderRadius: BorderRadius.circular(20),
+                    color: const Color.fromARGB(0, 255, 255, 255),
+                  ),
+                  child: const Column(
+                    children: [
+                      Row(children: [
+                                  Text(
+                                  'FOLLOWING',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color.fromARGB(255, 2, 160, 251))
+                                ),
+                                 SizedBox(width: 50),
+                                 Text(
+                                  'FOLLOWERS',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color.fromARGB(133, 0, 0, 0))
+                                ),
+                                ],
+                                
+                                ),
+                       Row(children: [
+                        CircleAvatar(
+                          radius: 15,
+                          backgroundColor: Color.fromARGB(255, 171, 112, 223),
+                        ),
+                        SizedBox(width: 5),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,  
+                                  children: [
+                                  Text(
+                                  'Hardi',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                  ),
+                                ),
+                                Text(
+                                  '4367 XP',
+                                  style: TextStyle(
+                                    fontSize: 10,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                                ],),
+                        SizedBox(width: 170),
+                        Icon(Icons.arrow_forward_ios_rounded, size: 16, color: Color.fromARGB(127, 0, 0, 0)),
+                      ]),
+                        const Row(children: [
+                        CircleAvatar(
+                          radius: 15,
+                          backgroundColor: Color.fromARGB(255, 223, 112, 112),
+                        ),
+                        SizedBox(width: 5),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,  
+                                  children: [
+                                  Text(
+                                  'Krishna',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                  ),
+                                ),
+                                Text(
+                                  '2334 XP',
+                                  style: TextStyle(
+                                    fontSize: 10,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                                ],),
+                        SizedBox(width: 155),
+                        Icon(Icons.arrow_forward_ios_rounded, size: 16, color: Color.fromARGB(127, 0, 0, 0)),
+                      ]),  
+                      
+        ],
+      ),
+    ),
+  ],
+),
             ],
           ),
         ),
@@ -98,62 +297,56 @@ class Profile extends StatelessWidget {
 
 } 
 
-class UnitCard extends StatelessWidget {
-  final double percentage;
-  const UnitCard({super.key, required this.percentage});
+class ProfileSection extends StatelessWidget {
+  const ProfileSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const Lessons()),
-        );
-      },
-      child: Container(
-        height: 160,
-        width: 140,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          color: const Color.fromARGB(80, 196, 196, 196),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(10, 10, 10, 30),
-          child: Stack(
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Align(
-                alignment: Alignment.topCenter,
-                child: Text(
-                  'Unit 1',
-                  style: TextStyle(fontSize: 22),
+              Text(
+                'Nidhi Pandya',
+                style: TextStyle(
+                  fontSize: 22,
                 ),
               ),
-              Progressbar(percentage: percentage),
+              Text(
+                'Nidhi12',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+              ),
+              SizedBox(height: 3),
+              Row(
+                children: [
+                  Icon(Icons.access_time_filled, size: 16, color: Color.fromARGB(127, 0, 0, 0)),
+                  SizedBox(width: 5),
+                  Text(
+                    'Joined March 2022',
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
-        ),
+          SizedBox(width: 77),
+          CircleAvatar(
+            radius: 35,
+            backgroundImage:  AssetImage("assets/images/profile.png"),
+          ),
+        ],
       ),
     );
   }
-}
-class LockedCard extends StatelessWidget {
-
-   const LockedCard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return  Container(
-        height: 160,
-        width: 140,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          color: const Color.fromARGB(80, 196, 196, 196),
-        ),
-          child: Padding(padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-          child: Image.asset("assets/images/locked.png"),
-          ),
-      );
-    }
 }
 
